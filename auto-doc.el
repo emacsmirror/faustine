b1e2bb072e280fd209ba817043784f9181b05bd6
yassinphilip <xaccrocheur@gmail.com>
2017-08-14 03:11:38 +0100

    Snippets:There are *already* snippets for Faust in MELPA :)

--- auto-doc.el
+++ auto-doc.el
@@ -11,7 +11,7 @@
 (defconst passing "[ ![Codeship Status for yassinphilip/faustine](https://app.codeship.com/projects/c2385cd0-5dc6-0135-04b2-0a800465306c/status?branch=master)](https://app.codeship.com/projects/238325)")
 
 (defun doc-a-mode ()
-  "Searches for next define-derived-mode and print markdown documentation."
+  "Search for next define-derived-mode and print markdown documentation."
   (interactive)
 
   (if (file-regular-p "README.md") (delete-file "README.md"))
@@ -19,28 +19,22 @@
   (with-temp-buffer
     (insert (documentation 'faust-mode))
     (goto-char (point-min))
-    (when (re-search-forward myregexp-key)
-      (setq heading (match-string 1)))
+    (when (re-search-forward myregexp-key) (setq heading (match-string 1)))
 
     (let* ((mylist (split-string (buffer-string) "^\\s-*$" t "\n"))
            (one (nth 0 mylist))
            (fiv (nth 4 mylist))
            (cmds (split-string fiv (rx
-                                    (and
-                                     line-start
-                                     (zero-or-more not-newline))
-                                    (submatch
-                                     "faustine"))))
+                                    (and line-start
+                                         (zero-or-more not-newline))
+                                    (submatch "faustine"))))
            (clean (split-string fiv (rx (and "\n")))))
 
       (with-temp-buffer
-        (insert "\# Faustine\n")
-        (insert (format "\n%s\n\n" heading))
-        (insert "\n---\n")
+        (insert (format "\# Faustine\n\n%s\n---\n" heading))
         (insert gpl3)
         (insert passing)
-
-        (insert "\n## Keys\n")
+        (insert "\n\n## Keys\n")
 
         (mapc (lambda (x)
                 (insert (format "\n- %s" x)))
@@ -65,12 +59,9 @@
           (beginning-of-line)
           (kill-line 2))
         (goto-char (point-min))
-
         (while (re-search-forward
                 (rx (and
                      "faustine.el")) nil t nil)
           (beginning-of-line)
           (kill-line 2)
-          (write-region (buffer-string) nil "README.md" nil 0 nil nil)
-          ;; (find-file "README.md")
-          )))))
+          (write-region (buffer-string) nil "README.md" nil 0 nil nil))))))
